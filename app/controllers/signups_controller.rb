f928246f4d69d39221ead92d679489efcb785a75
Kanda Mateta <matetah@gmail.com>
2020-04-05 17:59:27 -0400

    add more codes check the commit

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,10 @@
 class SignupsController < ApplicationController
+    def new
+        @signup = Signup.new
+    end
+    
+    private
+    def signup_params
+        params.require(:signup).permit(:email)
+    end
 end
